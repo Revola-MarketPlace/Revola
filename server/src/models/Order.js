b7d93e021e0440659b550adc6cf6118e52089030
petrossisay1646 <petrossisay1646@gmail.com>
2026-08-24 00:47:49 +0300

    adding order.js file

---
+++ server/src/models/Order.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    deliveryFee: {
+      type: Number,
+      default: 0, // Staff sets this manually or defaults to 0 before setting
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["TELEBIRR", "CHAPA", "BANK_TRANSFER", "MOCK"],
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: [
+        "PENDING_PAYMENT",
+        "PAYMENT_VERIFICATION",
+        "CONFIRMED",
+        "PROCESSING",
+        "READY_FOR_DELIVERY",
+        "OUT_FOR_DELIVERY",
+        "DELIVERED",
+        "CANCELLED",
+        "DISPUTED",
+        "REFUNDED",
+        "COMPLETED",
+      ],
+      default: "PENDING_PAYMENT",
+    },
+    paymentStatus: {
+      type: String,
+      enum: [
+        "PENDING",
+        "PROCESSING",
+        "PAID",
+        "FAILED",
+        "REFUNDED",
+        "PENDING_VERIFICATION",
+      ],
+      default: "PENDING",
+    },
+    deliveryStatus: {
+      type: String,
+      enum: [
+        "PENDING",
+        "ASSIGNED",
+        "PICKED_UP",
+        "OUT_FOR_DELIVERY",
+        "DELIVERED",
+        "FAILED",
+      ],
+      default: "PENDING",
+    },
+    deliveryAddress: {
+      streetAddress: { type: String, required: true },
+      subCity: { type: String, required: true },
+      city: { type: String, required: true },
+      phoneNumber: { type: String, required: true },
+    },
+    trackingNumber: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Order", orderSchema);
